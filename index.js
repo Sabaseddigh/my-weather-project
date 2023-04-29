7a54849b410f0df086601d722fc936cc42098c42
Saba <Sabaseddigh@users.noreply.github.com>
2023-04-30 02:34:38 +0330

    changing the api key.

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "bd5b4461863eddaa6ced0a0a67989e0a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -108,7 +108,7 @@ iconElement.setAttribute("alt", response.data.weather[0].description);
 
 
 function searchCity(city) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "bd5b4461863eddaa6ced0a0a67989e0a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
